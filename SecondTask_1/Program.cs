e300c53b8f1e2c7b834c7c8dfbfa8b51ade1c521
ChildOfJustice <izzvms@gmail.com>
2021-04-11 14:07:35 +0300

    .

---
+++ SecondTask_1/Program.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecondTask_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // foreach (var primeNumber in FindPrimeNumbers(100))
+            // {
+            //     Console.Write(primeNumber + ", ");
+            // }
+
+            // Console.WriteLine(Galois_b2_ext_mult(115, 183, 283));
+
+            
+            //Console.WriteLine(Galois_b2_ext_mult(FastPowMode(115, 254, 254), 115, 283));//283
+
+            uint b = 115;
+            uint res = 115;
+            for (int i = 1; i < 255; i++)
+            {
+            
+	            res = Galois_b2_ext_mult(res, b, 283);
+            }
+            
+            Console.WriteLine(res);
+
+
+            Console.WriteLine(Galois_b2_ext_mult(115, FastPowMode(115, 254, 256), 283));
+            //Console.WriteLine(Galois_b2_ext_mult(115, 183, 283));//283
+            //Console.WriteLine(FastPowMode(115, 282, 282));
+            
+            // Console.WriteLine(FastPowMode(595, 703, 991));
+        }
+
+        int Gcd(int a, int b, out int x, out int y)
+        {
+	        if (b < a)
+	        {
+		        var t = a;
+		        a = b;
+		        b = t;
+	        }
+    
+	        if (a == 0)
+	        {
+		        x = 0;
+		        y = 1;
+		        return b;
+	        }
+ 
+	        int gcd = Gcd(b % a, a, out x, out y);
+    
+	        int newY = x;
+	        int newX = y - (b / a) * x;
+    
+	        x = newX;
+	        y = newY;
+	        return gcd;
+        }
+        
+        static uint GetLeadBitNum(UInt32 Val) {
+	        if (0 == Val) {
+		        Console.WriteLine("Попытка найти старший бит числа \"0\"! Это ни к чему хорошему не приведёт."); return 0; }
+		        
+	        int BitNum = 31;
+	        uint CmpVal = 1u << BitNum;
+	        while (Val < CmpVal) {
+		        CmpVal >>= 1;
+		        BitNum--;
+	        }
+	        return (uint)BitNum;
+        }
+        
+        //https://habr.com/ru/post/528142/
+        public static uint Galois_b2_ext_mult(uint m1, uint m2, uint Poly = 283) {
+			if (0 == m1 || 0 == m2) { return 0; }
+			uint m1_tmp = m1;
+			uint m2_tmp;
+			uint m1_bit_num = 0;
+
+			//Перемножение двух полиномов, при использовании арифметики по модулю 2 достаточно простое занятие.
+			//перебираем единички и нолики (для каждого бита первого числа перебираем все биты второго (или наоборот)), складываем номера позиций битов,
+			//но не всегда, а только когда оба перебираемых бита - единицы, и инвертируем бит результата под номером, равном сумме позиций для данного шага перебора
+			//(инверсия - это прибавление единицы по модулю 2)
+			uint PolyMultRez = 0;
+
+			while (m1_tmp != 0) {
+				uint bit_m1 = (m1_tmp & 1u) == 0u ? 0u : 1u;
+				m1_tmp = m1_tmp >> 1;
+				m2_tmp = m2;
+				uint m2_bit_num;
+				m2_bit_num = 0;
+				while (m2_tmp != 0) {
+					uint bit_m2 = (m2_tmp & 1u) == 0u ? 0u : 1u;
+					m2_tmp = m2_tmp >> 1;
+					if ((bit_m1 != 0) && (bit_m2 != 0)) {
+						int BitNum = (int)(m2_bit_num + m1_bit_num);
+						PolyMultRez ^= 1u << BitNum;
+					}
+					m2_bit_num = m2_bit_num + 1;
+				}
+				m1_bit_num = m1_bit_num + 1;
+			}
+			
+			//Тут есть результат умножения полиномов PolyMultRez. Осталось найти остаток от деления на выбранный порождающий полином.
+			//Деление полиномов происходит так: Берём старшую степень делимого, и вычитаем старшую степень делителя. 
+			//Получаем число - степень частного
+			//Теперь перемножаем, а по сути, просто прибавляем к каждой степени делителя степень получившегося частного
+			//и повторяем всё по кругу, пока степень делимого не окажется меньше степени делителя
+			if(PolyMultRez > Poly)
+				return ModeDivide(PolyMultRez, Poly);
+			return PolyMultRez;
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			// uint TmpDivisor_lead_bit_n;
+			// uint TmpQuotient;
+			// uint TmpDivisor = Poly;
+			// uint TmpDividend = PolyMultRez;
+			// uint TmpDividend_LeadBitNum;
+			// uint TmpMult_bitNum;
+			// uint TmpMult_rez;
+
+			// TmpDividend_LeadBitNum = GetLeadBitNum(TmpDividend);
+			// TmpDivisor_lead_bit_n = GetLeadBitNum(TmpDivisor);
+
+			
+			uint tmpDivisor = Poly;
+			uint tmpDivision = PolyMultRez;
+			while (tmpDivision >= tmpDivisor)
+			{
+				
+			}
+			
+			
+			// while (TmpDividend_LeadBitNum >= TmpDivisor_lead_bit_n) {
+			//
+			// 	TmpQuotient = (TmpDividend_LeadBitNum - TmpDivisor_lead_bit_n);
+			//
+			// 	TmpMult_bitNum = 0;
+			// 	TmpMult_rez = 0;
+			// 	while (TmpDivisor != 0) {
+			// 		uint bit_TmpMult = (TmpDivisor & 1u) == 0u ? 0u : 1u;
+			// 		TmpDivisor >>= 1;
+			// 		TmpMult_rez ^= bit_TmpMult << (int)(TmpQuotient + TmpMult_bitNum);
+			// 		TmpMult_bitNum = TmpMult_bitNum + 1;
+			// 	}
+			// 	TmpDividend = TmpDividend ^ TmpMult_rez;
+			// 	TmpDivisor = Poly;
+			// 	TmpDividend_LeadBitNum = GetLeadBitNum(TmpDividend);
+			// }
+			//Результат умножения числел есть остаток от деления произведения многочленов на порождающий полином.
+			//return TmpDividend;
+			return 0;
+        }
+
+        public static uint GaloisReverse(uint b)
+        {
+	        return FastPowMode(b, 254, 256);
+        }
+
+        static uint ModeDivide(uint division, uint divisor)
+        {
+	        if (division < divisor)
+	        {
+		        throw new Exception("Cannot find the first non null bit when dividing by a greater value");
+	        }
+	        
+	        var maxNonNullBitForDivisor = GetMaxNonNullBitNumber(divisor);
+
+
+	        var tmpDivision = division;
+	        var currMaxNonNullBitNumber = GetMaxNonNullBitNumber(division);
+	        
+	        while (currMaxNonNullBitNumber >= maxNonNullBitForDivisor)
+	        {
+		        var tmpDivisor = divisor << (currMaxNonNullBitNumber-maxNonNullBitForDivisor);
+		        //Console.WriteLine("DIVIDING: " + tmpDivision + " - " + tmpDivisor);
+		        tmpDivision ^= tmpDivisor;
+		        currMaxNonNullBitNumber = GetMaxNonNullBitNumber(tmpDivision);
+		        //Console.WriteLine("ITER: " + tmpDivision);
+	        }
+
+	        return tmpDivision;
+        }
+        static int GetMaxNonNullBitNumber(uint number)
+        {
+	        var tmpDivision = number;
+	        int maxBitNumber = -1;
+	        while (tmpDivision > 0)
+	        {
+		        tmpDivision >>= 1;
+		        maxBitNumber++;
+	        }
+
+	        return maxBitNumber;
+        }
+
+
+
+
+        public static uint FastPowMode(uint number, uint pow, uint mode)
+        {
+	        uint result = 1;      
+	        while (pow != 0) {
+		        if (pow % 2 == 1)  result = Galois_b2_ext_mult(result, number, 283);
+		        pow >>= 1;
+		        result = Galois_b2_ext_mult(result, result, 283);
+	        }
+	        
+	        return result;
+	        
+	        
+	        
+	        
+	        // var numberInBase2 = new List<bool>();
+	        //
+	        // while (pow > 1)
+	        // {
+	        //  numberInBase2.Add(pow % 2 == 1);
+	        //  pow /= 2;
+	        // }
+	        // numberInBase2.Add(pow == 1);
+	        //
+	        // numberInBase2.Reverse();
+	        //
+	        //
+	        // var result = 1u;
+	        //
+	        // for (int i = 0; i < numberInBase2.Count; i++)
+	        // {
+	        //  if (numberInBase2[i])
+	        //  {
+	        //   // result *= number;
+	        //   // result %= mode;
+	        //   result = Galois_b2_ext_mult(result, number, 283);
+	        //   //Console.WriteLine("1! *= number: " + result);
+	        //  }
+	        //
+	        //  if (i != numberInBase2.Count - 1)
+	        //  {
+	        //   // result *= result;
+	        //   // result %= mode;
+	        //   result = Galois_b2_ext_mult(result, result, 283);
+	        //   //Console.WriteLine("^2: " + result);
+	        //  }
+	        //  
+	        // }
+	        // return result;
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        public static List<uint> FindPrimeNumbers(int n)
+        {
+            var numbers = new List<uint>();
+            //заполнение списка числами от 2 до n-1
+            for (var i = 2u; i < n; i++)
+            {
+                numbers.Add(i);
+            }
+
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                for (var j = 2u; j < n; j++)
+                {
+                    //удаляем кратные числа из списка
+                    numbers.Remove(numbers[i] * j);
+                }
+            }
+
+            return numbers;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+//https://ru.wikipedia.org/wiki/%D0%A0%D0%B5%D1%88%D0%B5%D1%82%D0%BE_%D0%AD%D1%80%D0%B0%D1%82%D0%BE%D1%81%D1%84%D0%B5%D0%BD%D0%B0
+//https://programm.top/c-sharp/programs/sieve-eratosthenes/
---
+++ SecondTask_2/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecondTask_2
+{
+    internal class Program
+    {
+        private static uint[,] A =
+        {
+            { 1, 0, 0, 0, 1, 1, 1, 1 },
+            { 1, 1, 0, 0, 0, 1, 1, 1 },
+            { 1, 1, 1, 0, 0, 0, 1, 1 },
+            { 1, 1, 1, 1, 0, 0, 0, 1 },
+            { 1, 1, 1, 1, 1, 0, 0, 0 },
+            { 0, 1, 1, 1, 1, 1, 0, 0 },
+            { 0, 0, 1, 1, 1, 1, 1, 0 },
+            { 0, 0, 0, 1, 1, 1, 1, 1 }
+        };
+        private static uint[,] revA =
+        {
+            { 0, 0, 1, 0, 0, 1, 0, 1 },
+            { 1, 0, 0, 1, 0, 0, 1, 0 },
+            { 0, 1, 0, 0, 1, 0, 0, 1 },
+            { 1, 0, 1, 0, 0, 1, 0, 0 },
+            { 0, 1, 0, 1, 0, 0, 1, 0 },
+            { 0, 0, 1, 0, 1, 0, 0, 1 },
+            { 1, 0, 0, 1, 0, 1, 0, 0 },
+            { 0, 1, 0, 0, 1, 0, 1, 0 }
+        };
+        
+        
+        public static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 256; i++)
+            {
+                if(i % 16 == 0)
+                    Console.WriteLine();
+                Console.Write(GetInversedSboxElement((uint)i).ToString("X") + " ");
+            }
+            
+
+
+            // foreach (var number in MakeReducedDeductionSystem(42))
+            // {
+            //     Console.Write(number + ", ");
+            // }
+        }
+        public static uint GetSboxElement(uint i)
+        {
+            // uint m = 0xf8;
+            //     
+            // var r = 0u;
+            // var b = 0u;
+            // if (i == 0)
+            //     b = 0;
+            // else b = SecondTask_1.Program.GaloisReverse(i);
+            //     
+            // for (var j = 0; j < 8; j++)
+            // {
+            //     r = (r << 1) | Xorbits(b & m);
+            //     m = (m >> 1) | ((m & 1) << 7);
+            // }
+            //     
+            // r ^= 0x63;
+
+
+            var currVector = new uint[8];
+            
+            uint currI = SecondTask_1.Program.GaloisReverse(i);
+            for (int k = 0; k < 8; k++)
+            {
+                currVector[k] = (currI >> k) & 1;
+            }
+            
+            var resVector = GaloisVectorMatrixMultiplication(A, currVector);
+            // foreach (var VARIABLE in resVector)
+            // {
+            //     Console.Write(VARIABLE + " ");
+            // }
+            // Console.Write("|||");
+            uint res = 0;
+            for (int k = 0; k < 8; k++)
+            {
+                res += (resVector[k] << k);
+            }
+            
+            res ^= 0x63;
+
+            return res;
+        }
+        public static uint GetInversedSboxElement(uint i)
+        {
+            for (int j = 0; j < 256; j++)
+            {
+                if (GetSboxElement((uint)j) == i)
+                    return (uint)j;
+            }
+
+            return 257;
+            //1
+            // var currVector = new uint[8];
+            //
+            // uint currI = SecondTask_1.Program.GaloisReverse(i);//SecondTask_1.Program.GaloisReverse
+            // for (int k = 0; k < 8; k++)
+            // {
+            //     currVector[k] = (currI >> k) & 1;
+            // }
+            //
+            // var resVector = GaloisVectorMatrixMultiplication(revA, currVector);
+            // // foreach (var VARIABLE in resVector)
+            // // {
+            // //     Console.Write(VARIABLE + " ");
+            // // }
+            // // Console.Write("|||");
+            // uint res = 0;
+            // for (int k = 0; k < 8; k++)
+            // {
+            //     res += (resVector[k] << k);
+            // }
+            //
+            // res ^= 0x5;
+            //
+            // // Console.Write(res.ToString("X") + " ");
+            // return res;
+        }
+
+        public static uint Xorbits(uint a)
+        {
+            uint res = 0;
+            while (a != 0)
+            {
+                res ^= (a & 1);
+                a >>= 1;
+            }
+
+            return res;
+        }
+        
+        
+        static uint[] GaloisVectorMatrixMultiplication(uint[,] inputMartix, uint[] vector)
+        {
+            uint[] resultMatrix;
+            
+  
+            resultMatrix = new uint[8];
+            
+            for (var i = 0; i < 8; i++)
+            {
+                resultMatrix[i] = 0;
+            
+                for (var k = 0; k < 8; k++)
+                {
+                    resultMatrix[i] ^= inputMartix[i, k] & vector[k];
+                }
+            }
+           
+
+            return resultMatrix;
+        }
+        
+        
+        static List<uint> MakeReducedDeductionSystem(uint m)
+        {
+            var result = new List<uint>();
+            result.Add(1);
+            for (int i = 2; i < m; i++)
+            {
+                if (EuclidAlgorithm(m, (uint)i) == 1)
+                {
+                    result.Add((uint)i);
+                }
+            }
+
+            return result;
+        }
+        
+        
+        
+        static uint EuclidAlgorithm(uint m, uint n)
+        {
+            while(m != n)
+            {
+                if(m > n)
+                {
+                    m = m - n;
+                }
+                else
+                {
+                    n = n - m;
+                }
+            }
+            return n;
+        }
+    }
+}
+
+//http://www.allmath.ru/highermath/algebra/theorychisel-ugu/17.htm
+//https://code-enjoy.ru/algoritm_evklida_na_c_sharp/
+

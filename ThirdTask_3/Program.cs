a20f7d555e229fb294f460490bcbf19eb30d547a
ChildOfJustice <izzvms@gmail.com>
2021-05-10 10:48:44 +0300

    Cleaned RSA.

--- ThirdTask_3/Program.cs
+++ ThirdTask_3/Program.cs
@@ -10,36 +10,27 @@ namespace ThirdTask_3
         
         public static void Main(string[] args)
         {
-            RsaCore rsa = new RsaCore(null);
-            rsa.keySize = 512;
-            
-            rsa.PrepareAlgorithm();
+            RsaCore rsa = new RsaCore(500);
 
             Console.WriteLine(rsa.GetPrivateKeyAsString(16));
             Console.WriteLine(rsa.GetPublicKeyAsString(16));
             
-            var data = new ASCIIEncoding().GetBytes("Secret");
-            BigInteger[] enc = new BigInteger[data.Length];
-            for (int i = 0; i < data.Length; i++)
-            {
-                enc[i] = rsa.Encrypt((int)data[i]);
-            }
+            var data = new ASCIIEncoding().GetBytes("Secret 2");
 
+            var encrypted = rsa.Encrypt(data);
+            
+            
             Console.WriteLine();
-            foreach (var VARIABLE in enc)
+            foreach (var VARIABLE in encrypted)
             {
                 Console.Write(" " + VARIABLE);
             }
             Console.WriteLine();
 
-
-            byte[] decr = new byte[enc.Length];
-            for (int i = 0; i < data.Length; i++)
-            {
-                decr[i] =(byte) rsa.Decrypt(enc[i]).IntValue();
-            }
-
-            Console.WriteLine("Decrypted data: " + new ASCIIEncoding().GetString(decr));
+            var decrypted = rsa.Decrypt(encrypted);
+            
+            
+            Console.WriteLine("Decrypted data: " + new ASCIIEncoding().GetString(decrypted));
         }
     }
 }
